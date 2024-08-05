07ff1ab99686cad2061c7a2ddbe6ad48c20cb517
Avijot-singh <singhavijot17@gmail.com>
2024-08-05 18:50:24 +1000

    JavaScript Initial Code

---
+++ JavaScript/VariableExcercise.js
@@ -0,0 +1,11 @@
+function test() {
+    var a = "3";
+    var b = "8";
+    /* not change the code above */
+    a = 8;
+    b = 3;
+
+
+    console.log("a is" + a);
+    console.log("b is" + b);
+}
