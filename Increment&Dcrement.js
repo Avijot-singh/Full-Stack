b5e075f5e72b3f9a44f662fbaffe2b0f7df34696
Avijot-singh <singhavijot17@gmail.com>
2024-08-06 12:15:31 +1000

    Incrementing and Decrementing numbers

---
+++ Increment&Dcrement.js
@@ -0,0 +1,15 @@
+// 
+var x = 5;
+x = x = 6;
+
+// x --1, x ++1
+
+var a = 10;
+var b = 5;
+a += b; // Result be 15
+
+// What does y  equal?
+var x = 3;  // X is given the value of 3
+var y = x++; // x++ is using post increment so the y is given the value of 3 first and then x is increment by 4
+y += 1; // Then x value is incremented by 1 
+// Y = 4
